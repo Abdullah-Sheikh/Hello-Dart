30e6dd8e66320ce8f089a5df1b0432b3bf583629
Abdullah <abdullahsh123@hotmail.com>
2020-12-02 00:41:54 +0500

    Numbers in Dart

--- bin/Dart_basics.dart
+++ bin/Dart_basics.dart
@@ -1,3 +1,5 @@
+//import 'dart:html';
+
 import 'package:Dart_basics/Dart_basics.dart' as Dart_basics;
 
 void main(List<String> arguments) {
@@ -13,5 +15,26 @@ void main(List<String> arguments) {
   var X = true;
 
   print ("X Data type is  ${X.runtimeType}");
+
+
+
+// numbers in Dart
+
+
+  num age = 21 ; // after assigning value they automatically decide their Data type
+
+  // Int in Dart
+
+  int x = 21 ;
+
+  int value = int.parse('21' , onError: ( sourse ) => 0) ;
+
+  print (value);
+
+  int v = int.parse('21.09' , onError: ( sourse ) => 0) ;
+
+  print (v);
+  
+  
 
 }
