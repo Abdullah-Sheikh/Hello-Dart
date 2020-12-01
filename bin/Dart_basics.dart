a937b32f5d839b3118a794239db3532d06f1b049
Abdullah <abdullahsh123@hotmail.com>
2020-12-01 21:01:20 +0500

    Add Bool var

--- bin/Dart_basics.dart
+++ bin/Dart_basics.dart
@@ -1,5 +1,17 @@
 import 'package:Dart_basics/Dart_basics.dart' as Dart_basics;
 
 void main(List<String> arguments) {
+
   print('Hello Dart !');
+
+  // Booleans
+
+  bool isOn = true;
+
+  print ("IsOn ${isOn}");
+
+  var X = true;
+
+  print ("X Data type is  ${X.runtimeType}");
+
 }
