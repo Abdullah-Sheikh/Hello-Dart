8effb91bbfb81e5e8a8f6461d1b4bfdd52543dce
Abdullah Sheikh <sheikhabdullahkurana@gmail.com>
2020-12-01 14:15:24 +0500

    Initial commit
    Signed-off-by: Abdullah <abdullahsh123@hotmail.com>

---
+++ bin/Dart_basics.dart
@@ -0,0 +1,5 @@
+import 'package:Dart_basics/Dart_basics.dart' as Dart_basics;
+
+void main(List<String> arguments) {
+  print('Hello Dart !');
+}
---
+++ test/Dart_basics_test.dart
@@ -0,0 +1,8 @@
+import 'package:Dart_basics/Dart_basics.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
